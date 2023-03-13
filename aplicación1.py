c9c9f4a0daf6c34d6a6f2eed12d6d12e46d80274
pandasaurus07 <114919763+pandasaurus07@users.noreply.github.com>
2023-03-13 13:45:29 +0100

    Add files via upload

---
+++ aplicación1.py
@@ -0,0 +1,26 @@
+ 
+temperatura = int(input("Dime la temperatura"))
+if(temperatura < 0):
+    print("Es hielo")
+elif(temperatura >= 100):
+    print("Es vapor")
+elif(temperatura < 100):
+    print("Líquido")
+                   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
