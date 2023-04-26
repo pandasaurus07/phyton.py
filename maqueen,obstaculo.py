b24410884512dd3412c3709bcc529518b45d4caa
pandasaurus07 <114919763+pandasaurus07@users.noreply.github.com>
2023-04-26 13:40:45 +0200

    Create maqueen,obstaculo.py

---
+++ maqueen,obstaculo.py
@@ -0,0 +1,24 @@
+# Imports go at the top
+from microbit import *
+from maqueen import *
+
+robot = Maqueen()
+
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_derecha()
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_izquierda()
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_izquierda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_derecha()
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_derecha()
+robot.mover_celda()
